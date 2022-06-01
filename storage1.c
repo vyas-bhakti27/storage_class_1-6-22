0f6f9ecece7bf8263d2b9307b6813c9142b407d5
vyas-bhakti27 <98168229+vyas-bhakti27@users.noreply.github.com>
2022-06-01 17:40:04 +0530

    Create storage1.c

---
+++ storage1.c
@@ -0,0 +1,13 @@
+static double y=15; // a global double variable
+
+float z=15; // global float variable 
+
+int main()
+{
+        auto int x=10;
+        static float a=10;
+        register int b=10;
+        return 1;
+}
+
+static void myFunction(void){}
